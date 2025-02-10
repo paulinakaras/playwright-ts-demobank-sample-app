a95bc7fb2ba10811cdc48e8556c8b8cf55d14736
paulinakaras <ppaulina.karas@gmail.com>
2025-02-10 17:36:32 +0100

    Fix flaky test

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -1,13 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Pulpit tests', () => {
-    test.describe.configure({ retries: 3 });
+    // test.describe.configure({ retries: 3 }); // configuration for flaky test
 
     test('quick payment with correct data', async ({ page }) => {
         await page.goto('https://demo-bank.vercel.app/');
         await page.getByTestId('login-input').fill('testerLO');
         await page.getByTestId('password-input').fill('asdfgh12');
         await page.getByTestId('login-button').click();
+
+        await page.waitForLoadState('domcontentloaded');
 
         await page.locator('#widget_1_transfer_receiver').selectOption('2');
         await page.locator('#widget_1_transfer_amount').fill('120');
